0c1b3046d66625e5596209607b5b656350f43ef0
Elchantaca <kikinchantaca@gmail.com>
2020-04-06 03:49:12 +0000

    Retos 11 y 12

---
+++ Reto12/12.cpp
@@ -0,0 +1,82 @@
+//Caso general:
+//Array A que consiste de N enteros
+//0 <= P < Q < N
+//La porción contiene al menos dos elementos
+//El promedio de una porción (P, Q) es la
+//suma de A[P] + A[P + 1] + ... + A[Q]
+//dividido entre el tamaño de la porción.
+//Para ser preciso, el promedio equivale a
+//(A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
+//La función debe retornar la posición de
+//inicio de la porción con el promedio mínimo
+
+
+
+//Caso particular:
+//Array A que consiste de N=7 enteros
+//A = [4,2,2,5,1,5,8]
+//0 <= P < Q < 7
+//La función debe retornar 1
+
+
+
+//Suposiciones
+//N es un entero dentro del rango [2..100,000];
+//Cada elemento del array A es un entero en el rango [−10,000..10,000].
+
+
+
+//Pseudocódigo:
+//1. Inicializar vector A
+//2. Inicializar prom_min = 10000
+//3. for (int P = 0; P < N-1; P++)
+    //3.1 for(int Q = P+1; Q < N; Q++)
+        //suma = 0
+        //3.1.1 for(int i = P; i <= Q; i++)
+            //suma = suma + A[i]
+
+
+
+        //promedio = suma/(Q-P+1)
+
+
+
+        //if (promedio < prom_min)
+            //prom_min = promedio
+            //pos = P
+//4. Retornar pos
+
+
+
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+int P,N,Q;
+
+
+int solution(vector<int> A){
+    for (int P = 0; P < N-1; P++){
+    for(int Q = P+1; Q < N; Q++){
+        for(int i = P; i <= Q; i++){
+            
+        }
+    }
+    
+}
+
+}
+
+
+
+
+int main(){
+    int res;
+    //Paso 1:
+    vector <int> A{4,4,3,4,2,1,3,2};
+    res = solution(A);
+    cout << res << endl;
+}
+
