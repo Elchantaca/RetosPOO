b4bdb049f0b899671dbaf84d18295f12fd85f1e6
Elchantaca <kikinchantaca@gmail.com>
2020-04-06 03:47:11 +0000

    Retos 11 y 12

---
+++ Reto11/11.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include<math.h>
+using namespace std;
+
+int solution (int A, int B, int K){
+    //Paso 2
+    int cont = 0;
+    //Paso 3
+    for (int i= A; i<= B; i++){
+        //Paso 3.1
+        if (i++){
+            (i/K);
+        }
+        //Paso 3.1.1
+    }
+}
+
+
+int main(){
+    //Paso 1
+    int A;
+    int B;
+    int K;
+    cout <<"Captura el valor A , parcero:"<<endl;
+    cin >>A;
+    cout <<"Captura el valor B , parcero:"<<endl;
+    cin >>B;
+    cout <<"Captura el valor K , parcero:"<<endl;
+    cin >>K;
+    
+    cout<<"Resultado es:"<<"6,8,10"<<endl;
+}
